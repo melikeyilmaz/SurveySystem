484b527676bb409a7ff6e704677834dc16e72fe2
melikeyilmaz <47854574+melikeyilmaz@users.noreply.github.com>
2023-09-24 01:54:39 +0300

    Soru Ekleme sayfasında tasarım.

--- SurveySystem/Models/Question.cs
+++ SurveySystem/Models/Question.cs
@@ -3,25 +3,25 @@
 namespace SurveySystem.Models
 {
     public class Question
-    {
-        //public int Id { get; set; }
-        //public string QuestionText { get; set; }
-
-        //[Required(ErrorMessage = "Doğru cevap seçeneği zorunludur.")]
-        //[Range(1, 5, ErrorMessage = "Doğru cevap seçeneği 1 ile 5 arasında bir değer olmalıdır.")]
-        //public int CorrectOptionId { get; set; }
-        //public List<Option> Options { get; set; } // Şık seçeneklerini bu liste içinde tutuyoruz
-        //public Option CorrectOption { get; set; }
-
+    { 
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Soru metni gereklidir.")]
         public string QuestionText { get; set; }
 
+        [Required(ErrorMessage = "1. Seçenek gereklidir.")]
         public string Option1 { get; set; }
+
+        [Required(ErrorMessage = "2. Seçenek gereklidir.")]
         public string Option2 { get; set; }
+
+        [Required(ErrorMessage = "3. Seçenek gereklidir.")]
         public string Option3 { get; set; }
+
+        [Required(ErrorMessage = "4. Seçenek gereklidir.")]
         public string Option4 { get; set; }
+
+        [Required(ErrorMessage = "5. Seçenek gereklidir.")]
         public string Option5 { get; set; }
 
         [Required(ErrorMessage = "Doğru cevap gereklidir.")]
