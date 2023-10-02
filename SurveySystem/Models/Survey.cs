b43c2dbb39cdde3ee796d40eb3d552e6999194b4
melikeyilmaz <47854574+melikeyilmaz@users.noreply.github.com>
2023-10-02 22:55:14 +0300

    Skor Tablosu sayfasına anket başlığı eklendi.

--- SurveySystem/Models/Survey.cs
+++ SurveySystem/Models/Survey.cs
@@ -30,33 +30,5 @@ namespace SurveySystem.Models
 
         // Üye olma durumu (bool)
         public bool IsMember { get; set; } 
-
-        //// Her bir ankete ait sonuç
-        //public List<SurveyScore>? SurveyScores { get; set; }
-
-
-        //public int Id { get; set; }
-
-        //[Required(ErrorMessage = "Ad gereklidir.")]
-        //public string FirstName { get; set; }
-
-        //[Required(ErrorMessage = "Soyad gereklidir.")]
-        //public string LastName { get; set; }
-
-        //// Birden fazla soru ve seçilen cevapları bu koleksiyon ile tutabilirsiniz.
-        //public List<QuestionResponse>? QuestionResponses { get; set; }
-
-        //// Anketin içerdiği soruların listesi
-        //public List<Question>? Questions { get; set; }
-
-        //// Benzersiz URL'yi saklamak için alan
-        //public string? UniqueId { get; set; }
-
-        //// Birden fazla soru ve seçilen cevapları bu koleksiyon ile tutabilirsiniz.
-        //public List<SurveyResponse>? SurveyResponses { get; set; }
-
-        //// Her bir anketin bir veya birden fazla SurveyScore kaydı olabilir
-        //public List<SurveyScore>? SurveyScores { get; set; }
-
     }
 }
