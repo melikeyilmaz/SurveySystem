1cabf8ca5d358e871929144204fa1a665add9016
melikeyilmaz <47854574+melikeyilmaz@users.noreply.github.com>
2023-10-03 19:00:12 +0300

    Yorum satırları devam.

--- SurveySystem/Models/QuestionResponse.cs
+++ SurveySystem/Models/QuestionResponse.cs
@@ -6,8 +6,7 @@
         public int SurveyId { get; set; } // Hangi ankete ait olduğunu belirten özellik
         public int QuestionId { get; set; }
         public int SelectedOption { get; set; } //Seçilen doğru cevap bilgisi
-
-        // 
+        
         public Question? Question { get; set; } // Soru ile ilişkiyi tanımlayın
         public Survey? Survey { get; set; }
     }
--- SurveySystem/Models/SurveyScore.cs
+++ SurveySystem/Models/SurveyScore.cs
@@ -12,14 +12,6 @@ namespace SurveySystem.Models
         public int CorrectAnswers { get; set; }
         public int IncorrectAnswers { get; set; }
 
-        // Foreign Key: Her bir SurveyScore kaydının hangi anketle ilişkilendirildiğini belirtmek için
-        // Hangi anketle ilişkilendirildiğini belirtmek için
-        //public int RelatedSurveyId { get; set; } 
-
-        // SurveyScore ile Survey arasındaki ilişki
-        //public int SurveyId { get; set; }
-        //public Survey? Survey { get; set; }
-
         // Her bir SurveyScore kaydının bir veya birden fazla SurveyResponse kaydı olabilir
         public List<SurveyResponse>? SurveyResponses { get; set; }
 
